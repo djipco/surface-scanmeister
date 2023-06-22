3d85bcf74b8756391c9260b1e2f40ddb3a2e8ea2
Jean-Philippe Côté <jp@djip.co>
2023-06-22 16:16:11 -0400

    upd

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ ScanMeister.init().then(() => {
 
     console.log(ScanMeister.devices);
 
-    // const opts = {deviceName: ScanMeister.devices[0].name};
-    // ScanMeister.scan(opts).pipe(fs.createWriteStream('./output.png'));
+    ScanMeister.devices.forEach(scanner => {
+        
+        // ScanMeister.scan(opts).pipe(fs.createWriteStream(`image${dev.index}.png`));
+        // console.log("called " + dev.index);
 
-    // ScanMeister.devices.forEach(dev => {
-    //     const opts = {deviceName: dev.name};
-    //     ScanMeister.scan(opts).pipe(fs.createWriteStream(`image${dev.index}.png`));
-    //     console.log("called " + dev.index);
-    // });
+        scanner.scan().pipe(fs.createWriteStream(`image${dev.index}.png`));
+    });
 
 });
