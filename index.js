5036d212c5c141f4c3ac291ef9fd8fb728197907
Jean-Philippe Côté <jp@djip.co>
2023-06-22 16:21:01 -0400

    upd

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ ScanMeister.init().then(() => {
         // console.log("called " + dev.index);
 
         scanner.scan().pipe(fs.createWriteStream(`image${scanner.index}.png`));
+        scanner.addListener("scancomplete", data => console.log("Complete: " + data))
+
     });
 
 });
