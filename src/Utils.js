e072f0c148799929468000dea7f54d9ea9e22eb0
Jean-Philippe Côté <jp@djip.co>
2023-06-26 15:13:37 -0400

    Add colors to error output

--- src/Utils.js
+++ src/Utils.js
@@ -4,14 +4,11 @@ export function getTimeStampString() {
   return format(new Date(), "[yyyy-MM-dd'T'HH:mm:ss]")
 }
 export function logError(text) {
-  // console.error(getTimeStampString(), text);
-
   console.error(getTimeStampString(), '\x1b[91m' + text + '\x1b[0m');
 }
 
 export function logWarn(text) {
-  // console.warn(getTimeStampString(), text);
-  console.error(getTimeStampString(), '\x1b[93m' + text + '\x1b[0m');
+  console.warn(getTimeStampString(), '\x1b[93m' + text + '\x1b[0m');
 }
 
 export function logInfo(text) {
