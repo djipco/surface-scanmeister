de6da9e1b18ec6a1b3006aa55fb4d7421ab7d0a6
Jean-Philippe Côté <jp@djip.co>
2023-08-02 00:24:36 -0400

    Rename "devices" to "scanners"

--- src/ScanMeister.js
+++ src/ScanMeister.js
@@ -148,11 +148,9 @@ class ScanMeister {
               // .filter(text => text.includes(config.get('devices.filter')))
               // .map(text => text.split('\n')[0]);
 
-            const re = /.*Bus=\s*(\d*).*Lev=\s*(\d*).*Prnt=\s*(\d*).*Port=\s*(\d*).*Cnt=\s*(\d*).*Dev#=\s*(\d*).*Vendor=(\S*).*ProdID=(\S*).*/gm
+            // const re = /.*Bus=\s*(\d*).*Lev=\s*(\d*).*Prnt=\s*(\d*).*Port=\s*(\d*).*Cnt=\s*(\d*).*Dev#=\s*(\d*).*Vendor=(\S*).*ProdID=(\S*).*/gm
 
-            // const re = /.*Bus=\s*(\d*).*Lev/
-
-            console.log(items[0]);
+            const re = /.*Bus=\s*(\d*).*Lev=\s*(\d*).*Prnt=\s*(\d*).*Port/
 
             const match = items[0].match(re);
 
