dcdd9ca8f49f4b228f0790332a2cfd9f750f2d60
Jean-Philippe Côté <jp@djip.co>
2023-08-02 16:10:35 -0400

    Use version in package.json

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ import {ScanMeister} from "./src/ScanMeister.js";
 import {logError, logInfo} from "./src/Utils.js";
 import process from 'node:process';
 
+import { readFile } from 'fs/promises';
+const pkg = JSON.parse(await readFile(new URL('./package.json', import.meta.url)));
+
+// Check platform
+if (process.platform !== "linux") {
+  logError(`This platform (${process.platform}) is not supported.`);
+  logInfo("Exiting...");
+  process.exit(1);
+}
+
 // Start ScanMeister
-logInfo(`Starting ScanMeister v${ScanMeister.version}...`)
+logInfo(`Starting ScanMeister v${pkg.version}...`);
 
 ScanMeister.init()
   .then(() => {
-    logInfo(`ScanMeister v${ScanMeister.version} successfully started`);
+    logInfo(`ScanMeister successfully started`);
     process.on('SIGINT', onExit);  // CTRL+C
     process.on('SIGQUIT', onExit); // Keyboard quit
     process.on('SIGTERM', onExit); // `kill` command
