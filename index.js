5f3e1f9ef390bcf18f1fa29009eb390546d2b9f1
Jean-Philippe Côté <jp@djip.co>
2024-02-25 16:59:28 -0500

    Merge remote-tracking branch 'origin/main'

--- index.js
+++ index.js
@@ -1,33 +1,10 @@
 // Import modules
-import {access, constants} from "fs/promises";
-import process from "node:process";
+import ScanMeister from "./src/ScanMeister.js";
+import {logError} from "./src/Logger.js";
 
-// Check if modules have been properly installed
-
-
-async function kickIt() {
-
-  let ScanMeister;
-
-  try {
-    await access("node_modules", constants.F_OK);
-    ScanMeister = await import("./src/ScanMeister.js")
-  } catch (error) {
-    console.error(`The modules have not been installed. Install them with: npm install`);
-    console.log("Exiting...");
-    setTimeout(() => process.exit(1), 500); // wait for log files to be written
-  }
-
-  // import ScanMeister from "./src/ScanMeister.js";
-
-  const scanmeister = new ScanMeister();
-  scanmeister.start()
-    .catch(async error => {
-      console.error(error);
-      await scanmeister.quit(1);
-    });
-
-}
-
-kickIt();
-
+const scanmeister = new ScanMeister();
+scanmeister.start()
+  .catch(async error => {
+    logError(error);
+    await scanmeister.quit(1);
+  });
