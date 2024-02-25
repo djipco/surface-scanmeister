029bef0433a54c60dd546da30509b979cc0b6e37
Jean-Philippe Côté <jp@djip.co>
2024-02-25 17:05:33 -0500

    Merge remote-tracking branch 'origin/main'

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 import ScanMeister from "./src/ScanMeister.js";
 import {logError} from "./src/Logger.js";
 
+
+console.log(process.env.NODE_MODULE_PATH);
+
 const scanmeister = new ScanMeister();
 scanmeister.start()
   .catch(async error => {
