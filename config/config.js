7a3106feddc1e3ebabd0d6f49a592548c6fde994
Jean-Philippe Côté <jp@djip.co>
2024-02-26 21:38:43 -0500

    Merge remote-tracking branch 'origin/main'

--- config/config.js
+++ config/config.js
@@ -101,12 +101,12 @@ const config = convict({
     width: {
       doc: 'Width of scan area (0...216.7mm)',
       format: 'Number',
-      default: 216.7
+      default: 216
     },
     height: {
       doc: 'Height of scan area (0...297.5mm)',
       format: 'Number',
-      default: 297.5
+      default: 297
     },
   }
 
