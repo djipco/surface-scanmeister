f74c955060cd1611be5b94f5ac6073274f2de973
Jean-Philippe Côté <jp@djip.co>
2024-02-28 08:31:03 -0500

    Check for node_modules at start

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-// Import modules
-import ScanMeister from "./src/ScanMeister.js";
-import {logError} from "./src/Logger.js";
+// Import necessary builtin modules
+import { existsSync } from 'fs';
 
-const scanmeister = new ScanMeister();
-scanmeister.start()
-  .catch(async error => {
-    logError(error);
-    await scanmeister.quit(1);
-  });
+// Check if external modules have been installed by looking for the 'node_modules' folder. If it
+// exists, start normally. If not, display error with instructions.
+if (existsSync('./node_modules')) {
 
+  const ScanMeister = (await import(`./src/ScanMeister.js`)).default;
+  const scanmeister = new ScanMeister();
+  scanmeister.start()
+    .catch(async error => {
+      console.error(`\x1b[91m ${error} \x1b[0m`);
+      await scanmeister.quit(1);
+    });
 
-// import { existsSync } from 'fs';
-//
-// if (!existsSync('./node_modules')) {
-//   console.error(`Error: node_modules directory missing`);
-// } else {
-//
-// }
-//
-// const value = (
-//   await import(`${condtion ? `./file1.js` : `./file2.js`}`)
-// ).default
+} else {
+
+  console.error(
+    `\x1b[91m Error: Modules have not been installed. ` +
+    `To install them, use 'npm install' in a Terminal at root of project.\x1b[0m`
+  );
+
+}
