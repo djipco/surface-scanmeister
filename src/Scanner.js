916bcf43f069cfac3b9165e9e191d62005e53e16
Jean-Philippe Côté <jp@djip.co>
2024-02-28 09:37:37 -0500

    Check for node_modules at start

--- src/Scanner.js
+++ src/Scanner.js
@@ -180,19 +180,18 @@ export class Scanner extends EventEmitter {
 
 
 
-    console.log(`scanimage ${this.#scanArgs.join(" ")} | nc -q 0 10.0.0.200 1234`);
 
-    // this.scanImageSpawner.execute(
-    //   `scanimage ${this.#scanArgs.join(" ")} | nc -q 0 10.0.0.200 1234`,
-    //   [],
-    //   {
-    //     detached: false,
-    //     shell: false,
-    //     sucessCallback: this.#onScanImageEnd.bind(this),
-    //     errorCallback: this.#onScanImageError.bind(this),
-    //     stderrCallback: this.#onScanImageStderr.bind(this)
-    //   }
-    // );
+    this.scanImageSpawner.execute(
+      `scanimage ${this.#scanArgs.join(" ")} | nc -q 0 10.0.0.200 1234`,
+      [],
+      {
+        detached: true,
+        shell: true,
+        sucessCallback: this.#onScanImageEnd.bind(this),
+        errorCallback: this.#onScanImageError.bind(this),
+        stderrCallback: this.#onScanImageStderr.bind(this)
+      }
+    );
 
 
 
