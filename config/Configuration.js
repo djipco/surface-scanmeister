b8e9e6e25c873d74a02dbab99e5f0a1c40f9d487
Jean-Philippe Côté <jp@djip.co>
2024-03-03 16:03:49 -0500

    Update

--- config/Configuration.js
+++ config/Configuration.js
@@ -6,11 +6,11 @@ export const Configuration = {
   },
 
   operation: {
-    mode: 'file'                    // Save scan locally (file) or send it via network (tcp)
+    mode: 'file'                    // Save scan locally (file) or stream them via network (tcp)
   },
 
   tcp: {
-    address: "10.0.0.200",          // Address of the remote TCP socket server
+    address: "10.0.0.200",          // Address of the remote TCP socket server to stream images to
     port: "1234"                    // Port of the remote server
   },
 
@@ -18,7 +18,7 @@ export const Configuration = {
     localAddress: '0.0.0.0',        // Local IP address to bind to for OSC
     localPort: 8000,                // Local port to listen on for OSC
     remoteAddress: '10.0.0.200',    // Remote IP address to send OSC to
-    remotePort: 10000               // Remote port to send OSC to
+    remotePort: 10000               // Remote port to send OSC on
   },
 
   devices: {
@@ -28,15 +28,15 @@ export const Configuration = {
     mapping: "Atolla16PortBus1Port1Dynex7PortBus3Port1", // Name of the mapping to use
     // mapping: "Atolla16PortBus1Port1",
 
-    resolution: 150,                // [75, 100, 150, 300, 600, 1200, 2400, 4800]
-    brightness: 25,                 // The scanning brightness (-100...100)
-    contrast: 25,                   // The scanning contrast (-100...100)
-    lampOffScan: false,             // Whether to close the lamp while scanning
-    lampOffTime: 15,                // Number of min. after which the lamp is turned off (0...60)
-    x: 0,                           // The x position to start the scan at (0...216.7mm)
-    y: 0,                           // The y position to start the scan at (0...297.5mm)
-    width: 216.07,                  // Width of scan area (0...216.7mm)
-    height: 297.5,                  // Height of scan area (0...297.5mm)
+    resolution: 150,              // Scanning resolution (75, 100, 150, 300, 600, 1200, 2400, 4800)
+    brightness: 25,               // Scanning brightness (-100...100)
+    contrast: 25,                 // Scanning contrast (-100...100)
+    lampOffScan: false,           // Whether to shut off lamp while scanning
+    lampOffTime: 15,              // Delay after which the lamp is turned off (0...60 min.)
+    x: 0,                         // Horizontal position to start the scan at (0...216.7mm)
+    y: 0,                         // Vertical position to start the scan at (0...297.5mm)
+    width: 216.07,                // Width of scan area (0...216.7mm)
+    height: 297.5,                // Height of scan area (0...297.5mm)
 
   }
 
