40f1479d73aac4c821c6e7a1b6ebd1721cb5e978
Jean-Philippe Côté <jp@djip.co>
2024-03-29 11:05:35 -0400

    Update

--- externals/send_distances.py
+++ externals/send_distances.py
@@ -11,7 +11,7 @@ import adafruit_vl6180x
 from pythonosc.udp_client import SimpleUDPClient
 
 # Internal imports
-from vl6180x_multi import MultiSensor
+from vl6180x_multi import VL6180xSensorCollection
 
 
 
@@ -43,11 +43,16 @@ gain = getattr(adafruit_vl6180x, "ALS_GAIN_" + args.gain.replace(".", "_", 1))
 # Create OSC client
 client = SimpleUDPClient(args.ip, args.port)  # Create client
 
-#
-# i2c = busio.I2C(board.SCL, board.SDA)
-ms = MultiSensor([17, 16])   # Using GPIO 16 and 17
 
-# 22 Usable GPIOs: 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 16, 17, 18, 19, 20, 21, 26, 22, 23, 24, 25, 27
+# i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create the sensor collection using the specified pins (in order). As far as I can tell, these are
+# the pins that can be used (26 in total):
+#   00, 01, 04, 05, 06, 07, 08, 09, 10, 11,
+#   12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+#   22, 23, 24, 25, 26, 27
+# collection = VL6180xSensorCollection([17, 16])
+collection = VL6180xSensorCollection([])
 
 
 # sensor = adafruit_vl6180x.VL6180X(i2c)    # Hardware id is 0x29 (41)
@@ -55,12 +60,11 @@ ms = MultiSensor([17, 16])   # Using GPIO 16 and 17
 
 while True:
 
-    for index, sensor in enumerate(ms.sensors):
-        distance = ms.sensors[index].range
-        luminosity = ms.sensors[index].read_lux(gain)
+    for index, sensor in enumerate(collection.sensors):
+        distance = collection.sensors[index].range
+        luminosity = collection.sensors[index].read_lux(gain)
         client.send_message(f"/sensor/{index}/distance", distance)
         client.send_message(f"/sensor/{index}/luminosity", luminosity)
-        print(index, distance, luminosity)
 
 
     # Select channel on multiplexer
@@ -74,8 +78,8 @@ while True:
 #     luminosity = sensor.read_lux(gain)
 
     # Send data via OSC
-#     client.send_message(f"/sensor/{channel}/distance", distance)
-#     client.send_message(f"/sensor/{channel}/luminosity", luminosity)
+#     client.send_message(f"/sensor/{channel}/distance", distance) # in mm
+#     client.send_message(f"/sensor/{channel}/luminosity", luminosity) # in lux
 #     client.send_message(f"/sensor/{channel}/distance", 123)
 #     client.send_message(f"/sensor/{channel}/luminosity", 45)
 
--- externals/vl6180x_multi.py
+++ externals/vl6180x_multi.py
@@ -1,17 +1,40 @@
-"""Multisensor main module.
+"""
+vl6180x_multi module.
 
-This module implements a class for managing multiple identical I2C devices of the same type using
-the same I2C bus. The solution is to reallocate them to the different addresses while taking them
-out of reset one by one.
+This module exposes a class allowing the creation and management of multiple VL6180x sensors on the
+same I2C bus. By default, this is not possible since the devices all have the same address (0x29).
+To circumvent that, the devices are assigned new addresses.
 
+Thanks to Vitaly Grinberg for making the original code available under an MIT licence:
 https://gitlab.com/vitus133/vl6180x_multi
+
+Requirements:
+
+    - Adafruit-Blinka==5.7.0
+    - adafruit-circuitpython-busdevice==5.0.1
+    - adafruit-circuitpython-lis3dh==5.1.5
+    - adafruit-circuitpython-vl6180x==1.2.3
+    - Adafruit-PlatformDetect==2.21.0
+    - Adafruit-PureIO==1.1.7
+    - colorzero==1.1
+    - gpiozero==1.5.1
+    - pigpio==1.78
+    - pkg-resources==0.0.0
+    - pyftdi==0.52.0
+    - pyserial==3.5
+    - pyusb==1.1.0
+    - rpi-ws281x==4.2.5
+    - RPi.GPIO==0.7.0
+    - sysv-ipc==1.0.1
 
 """
 
+# Standard imports
 import time
 import os
 import json
 
+# External imports
 import board
 import busio
 import adafruit_vl6180x
@@ -21,28 +44,39 @@ try:
 except RuntimeError:
     print("Error importing RPi.GPIO! Superuser privileges required!")
 
-""" Register for changing the device bus address """
-SUBORDINATE_ADDR_REG = 0x212
+# CONSTANTS
+SUBORDINATE_ADDR_REG = 0x212        # Register to change the device address
+DEFAULT_SENSOR_ADDRESS = 0x29       # Default VL6180X I2C address (41)
 
-""" Default address sensor gets out of reset with """
-DEFAULT_SENSOR_ADDRESS = 41
-
-
-class MultiSensor():
+# VL6180xSensorCollection class definition
+class VL6180xSensorCollection():
 
     def __init__(self, ce_gpios: list, start_addr=None):
 
         try:
+
+            # Initialize empty sensor list
             self.sensors = []
+
+            # Assign channel list
+            self.channels = ce_gpios
+
+            # Assign I2C address for the first device
             if start_addr is not None and start_addr != DEFAULT_SENSOR_ADDRESS:
                 self.start_addr = start_addr
             else:
                 self.start_addr = DEFAULT_SENSOR_ADDRESS + 1
 
+            # Use Broadcom SOC channel numbers for pin numbers. This is the number found beside GPIO
+            # such as GPIO17 and NOT the pin numbers printed on the board.
             GPIO.setmode(GPIO.BCM)
-            self.channels = ce_gpios
+
+            # Make all pins outputs and initially set them to low so we can reallocate their I2C
+            # address before activating them.
             GPIO.setup(self.channels, GPIO.OUT)
             GPIO.output(self.channels, GPIO.LOW)
+
+            # Reallocate I2C addresses
             self._realloc_addr()
 
         except Exception as e:
@@ -50,41 +84,39 @@ class MultiSensor():
             exit(1)
 
     def _realloc_addr(self):
-        """ Reallocate default I2C addresses
-        All the sensors are now shut down (done in the constructor)
-        Turn them on one by one and reassign to another address
-        """
+
+        # Create I2C bus and get a list of I2C addresses that cannot be used because they are
+        # already in use.
         i2c = busio.I2C(board.SCL, board.SDA)
         busy_addr = i2c.scan()
-        # Default address is 41 (0x29). This is the address our sensors get out of reset.
+
+        # To be on the safe side, we check if the default address (0x29) is in the found devices
+        # list. This could be because a device could not be deactivated by setting the output to
+        # GPIO.LOW.
         if DEFAULT_SENSOR_ADDRESS in busy_addr:
-            raise RuntimeError(f"I2C address conflict, please check GPIO")
+            raise RuntimeError(f"I2C address conflict, please check GPIO.")
+
+        # Assign addresses by starting at start address and finding next available addresses
         next_addr = self.start_addr
         for channel in self.channels:
-            # find next unoccupied address
+
             while next_addr in busy_addr:
                 next_addr += 1
                 if next_addr > 127:
                     next_addr = 0
                 if len(busy_addr) >= 128:
                     raise RuntimeError("Ran out of I2C addresses")
-            # Allocate the next address to the sensor
+
             busy_addr.append(next_addr)
 
-            # Turn on the sensor
+            # Now that the new address is set, we can activate the sensor. As per the device
+            # documentation, we need to wait at least 400μs after activation.
             GPIO.output(channel, GPIO.HIGH)
-
-            # Hitting an error without this delay
-            # Adafruit_PureIO/smbus.py", line 308, in write_bytes
-            # self._device.write(buf) OSError:
-            # [Errno 121] Remote I/O error
             time.sleep(0.1)
 
-            # Instantiate as temporary
+            # Because of the way the adafruit_vl6180x library works, we first need to create a dummy
+            # device and assign it it's new address. Then, we can create the final sensor object and
+            # the actual I2C address we want to use.
             temp = adafruit_vl6180x.VL6180X(i2c)
-
-            # Change the address to the assigned one
             temp._write_8(SUBORDINATE_ADDR_REG, next_addr)
-
-            # Instantiate the sensor with the new address
             self.sensors.append(adafruit_vl6180x.VL6180X(i2c, address=next_addr))
