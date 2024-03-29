98eba3435d65975094be07fc78ec52c4fea9b401
Jean-Philippe Côté <jp@djip.co>
2024-03-29 13:37:22 -0400

    Update

--- externals/send_distances.py
+++ externals/send_distances.py
@@ -24,10 +24,15 @@ from vl6180x_multi import VL6180xSensorCollection
 
 # Construct the argument parser and configure available arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--ip", default="127.0.0.1", help="IP address of the OSC target")
-ap.add_argument("-p", "--port", default=10000, type=int, help="Port of the OSC target")
+ap.add_argument("-i", "--ip", default="127.0.0.1", help="IP address of the OSC target.")
+ap.add_argument("-p", "--port", default=10000, type=int, help="Port of the OSC target.")
 ap.add_argument("-g", "--gain", default="1", help="Gain to apply to luminosity [1, 1.25, 1.67, 2.5, 5, 10, 20, 40]")
+ap.add_argument("-n", "--pins", default="1", help="GPIO pin numbers where the sensors are connected (comma-separated).")
 args = ap.parse_args()
+
+# Parse pin numbers into list
+pins = [int(item) for item in args.pins.split(',')]
+print(pins)
 
 # Parse luminosity gain according to following constants:
 #   - adafruit_vl6180x.ALS_GAIN_1       = 1x        # 6
@@ -52,7 +57,11 @@ client = SimpleUDPClient(args.ip, args.port)  # Create client
 #   12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
 #   22, 23, 24, 25, 26, 27
 # collection = VL6180xSensorCollection([17, 16])
-collection = VL6180xSensorCollection([])
+collection = VL6180xSensorCollection([
+     0,  1,  4,  5,  6,  7,  8,  9, 10, 11,
+    12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27
+])
 
 
 # sensor = adafruit_vl6180x.VL6180X(i2c)    # Hardware id is 0x29 (41)
