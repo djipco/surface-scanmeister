4ec5f2c0a70105830d2f7bb574deac44557ae6ff
Jean-Philippe Côté <jp@djip.co>
2024-04-03 19:10:59 -0400

    Update

--- externals/send_distances.py
+++ externals/send_distances.py
@@ -32,7 +32,7 @@ args = ap.parse_args()
 
 # Parse pin numbers into list
 pins = [int(item) for item in args.pins.split(',')]
-print(pins)
+print("pins", pins)
 
 # Parse luminosity gain according to following constants:
 #   - adafruit_vl6180x.ALS_GAIN_1       = 1x        # 6
@@ -56,7 +56,7 @@ client = SimpleUDPClient(args.ip, args.port)  # Create client
 #   00, 01, 04, 05, 06, 07, 08, 09, 10, 11,
 #   12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
 #   22, 23, 24, 25, 26, 27
-collection = VL6180xSensorCollection([4])
+collection = VL6180xSensorCollection(pins)
 # collection = VL6180xSensorCollection([
 #      0,  1,  4,  5,  6,  7,  8,  9, 10, 11,
 #     12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
@@ -72,8 +72,9 @@ while True:
     for index, sensor in enumerate(collection.sensors):
         distance = collection.sensors[index].range
         luminosity = collection.sensors[index].read_lux(gain)
-        client.send_message(f"/sensor/{index}/distance", distance)
-        client.send_message(f"/sensor/{index}/luminosity", luminosity)
+#         client.send_message(f"/sensor/{index}/distance", distance)
+#         client.send_message(f"/sensor/{index}/luminosity", luminosity)
+        print(distance, luminosity)
 
 
     # Select channel on multiplexer
