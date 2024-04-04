ed4d913d2b388d2b3856d46a42a28cc30fc09fab
Jean-Philippe Côté <jp@djip.co>
2024-04-04 16:57:06 -0400

    Close parent window when exiting

--- src/ScanMeister.js
+++ src/ScanMeister.js
@@ -372,7 +372,7 @@ export default class ScanMeister {
 
     // Exit
     setTimeout(() => {
-      process.kill(process.ppid, 'SIGHUP');
+      process.kill(process.ppid, 'SIGKILL');
       // process.exit(status);
     }, 100); // wait for log files to be written
 
