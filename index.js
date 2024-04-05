b4444978c515ed9c0d2becf222da0aa9557ff26d
Jean-Philippe Côté <jp@djip.co>
2024-04-04 21:03:52 -0400

    Close parent window when exiting

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
 
+
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 console.log(__dirname);
 
 // try {
