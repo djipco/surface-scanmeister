20cbc5a0a95251f5d2c0725dd89869fdf710a08a
Jean-Philippe Côté <jp@djip.co>
2024-04-05 13:23:50 -0400

    Remove buffering from scanimage

--- src/Scanner.js
+++ src/Scanner.js
@@ -159,7 +159,8 @@ export class Scanner extends EventEmitter {
     this.scanImageSpawner = new Spawner();
 
     this.scanImageSpawner.execute(
-      "stdbuf --output=0 scanimage",
+      // "stdbuf --output=0 scanimage",
+      "scanimage",
       this.#scanArgs,
       {
         detached: false,
