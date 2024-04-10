2b8c78d4b198bf13869cc94eddb504ba2487e818
Jean-Philippe Côté <jp@djip.co>
2024-04-10 14:21:37 -0400

    Update

--- src/LightSensors.js
+++ src/LightSensors.js
@@ -32,12 +32,12 @@ export class LightSensors extends EventEmitter {
     // // logInfo(ports);
     // // const arduinoPort = ports.find(port => port.manufacturer?.includes('Arduino'));
     // // return arduinoPort ? arduinoPort.path : null;
-    //
-    // // Set up serial connection and line parser
-    // this.#port = new SerialPort('/dev/ttyACM0', {baudRate: 115200});
+
+    // Set up serial connection and line parser
+    this.#port = new SerialPort('/dev/ttyACM0', {baudRate: 115200});
     // this.#parser = this.#port.pipe(new ReadlineParser({delimiter: '\n'}));
-    //
-    // // Listen for 'data' event
+
+    // Listen for 'data' event
     // this.#callbacks.onData = this.#onData.bind(this);
     // this.#parser.on('data', this.#callbacks.onData);
 
