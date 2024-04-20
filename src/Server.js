fc23057ae3cb16f4b45673984e70f47cc4f935e5
Jean-Philippe Côté <jp@djip.co>
2024-04-20 17:55:20 -0400

    Update

--- src/Server.js
+++ src/Server.js
@@ -1,10 +1,9 @@
 // Node.js standard imports
 import http from 'node:http';
-import fs from 'fs';
-import path from 'path';
+import express from 'express';
 
-import { fileURLToPath } from 'url'
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+// import { fileURLToPath } from 'url'
+// const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // Application imports
 import {logError, logInfo, logWarn} from "./Logger.js";
@@ -20,7 +19,8 @@ export class Server extends EventEmitter {
   // Private members
   #callbacks = {};                  // callback functions used in this class
   #clients = [];                    // List of clients that requested a scan
-  #httpServer = undefined;          // HTTP Server
+  #httpScannerServer = undefined;   // HTTP Server (5678)
+  #express = undefined;             // Express server, for static files (8080)
   #scanners = undefined;            // List of available scanners
 
   constructor() {
@@ -34,10 +34,6 @@ export class Server extends EventEmitter {
     response.setHeader('Access-Control-Request-Method', '*');
     response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
     response.setHeader('Access-Control-Allow-Headers', '*');
-
-    const result = await this.handleStaticFileRequests(request, response);
-    console.log(result);
-    if (result) return;
 
     // Parse the path of the URL and split it into segments
     const url = new URL(request.url, `http://${request.headers.host}`);
@@ -151,16 +147,39 @@ export class Server extends EventEmitter {
     this.#scanners.forEach(async scanner => await scanner.abort());
 
     // Remove events and stop the HTTP Server
-    if (this.#httpServer) {
-      this.#httpServer.removeAllListeners();
-      this.#httpServer.close();
-      this.#httpServer.closeAllConnections();
-      this.#httpServer.unref();
+    if (this.#httpScannerServer) {
+      this.#httpScannerServer.removeAllListeners();
+      this.#httpScannerServer.close();
+      this.#httpScannerServer.closeAllConnections();
+      this.#httpScannerServer.unref();
     }
+
+    // Stop Express server
+    await new Promise(resolve => {
+      this.#express.close(() => resolve);
+    });
 
   }
 
   async start(scanners, options = {address: "0.0.0.0", port: 5678}) {
+
+    // Set up server for static client files (Express) and specify the directory to serve files from
+    this.#express = express();
+    this.#express.use(express.static('../webclient'));
+
+    // // Optional: Define a fallback route for handling requests to non-existent files
+    // this.#express.use((req, res) => {
+    //   res.status(404).send('File not found!');
+    // });
+
+    // Start the server
+    this.#express.listen(8080, () => {
+      console.log(`Server is running on http://localhost: 8080`);
+    });
+
+
+
+
 
     if (!Array.isArray(scanners)) {
       logError("An array of Scanner objects must be specified to start the Server.");
@@ -170,16 +189,16 @@ export class Server extends EventEmitter {
     this.#scanners = scanners;
 
     // Create HTTP server and add callbacks
-    this.#httpServer = http.createServer();
+    this.#httpScannerServer = http.createServer();
     this.#callbacks.onHttpRequest = this.#onHttpRequest.bind(this);
-    this.#httpServer.on("request", this.#callbacks.onHttpRequest);
+    this.#httpScannerServer.on("request", this.#callbacks.onHttpRequest);
     this.#callbacks.onServerError = this.#onServerError.bind(this);
-    this.#httpServer.on("error", this.#callbacks.onServerError);
+    this.#httpScannerServer.on("error", this.#callbacks.onServerError);
 
     // Start server
     await new Promise((resolve, reject) => {
 
-      this.#httpServer.listen(options, err => {
+      this.#httpScannerServer.listen(options, err => {
         if (err) {
           reject("Could not start HTTP server. " + err);
           this.quit();
@@ -192,57 +211,57 @@ export class Server extends EventEmitter {
 
   }
 
-  async handleStaticFileRequests(req, res) {
-
-    const mimeTypes = {
-      '.html': 'text/html',
-      '.css': 'text/css',
-      '.js': 'application/javascript',
-      '.ico': 'image/x-icon',
-      '.json': 'application/json',
-      '.map': 'application/json',
-      '.png': 'image/png',
-      '.jpg': 'image/jpeg',
-      '.gif': 'image/gif',
-      '.svg': 'image/svg+xml'
-    };
-
-
-    // If it's not the GET method, let the default process handle it
-    if (req.method !== 'GET') return false;
-
-    let fileUrl = req.url;
-    if (fileUrl === '/') fileUrl = '/index.html';
-    const filePath = path.resolve(__dirname, "../webclient/" + fileUrl);
-    const fileExt = path.extname(filePath);
-    const mimeType = mimeTypes[fileExt];
-
-    console.log(fileUrl, filePath);
-
-    // If it's not the right file extension, let the default process handle it
-    // if (!Server.ALLOWED_STATIC_FILE_EXTENSIONS.includes(fileExt)) return false;
-    if (!mimeType) return false;
-
-
-    return new Promise(resolve => {
-
-      fs.access(filePath, fs.constants.F_OK, err => {
-
-        // If it's not an existing file, let the default process handle it
-        if (err) {
-          resolve(false);
-        } else {
-          res.statusCode = 200;
-          res.setHeader('Content-Type', mimeType);
-          fs.createReadStream(filePath).pipe(res);
-          resolve(true);
-        }
-
-      });
-
-    })
-
-  }
+  // async handleStaticFileRequests(req, res) {
+  //
+  //   const mimeTypes = {
+  //     '.html': 'text/html',
+  //     '.css': 'text/css',
+  //     '.js': 'application/javascript',
+  //     '.ico': 'image/x-icon',
+  //     '.json': 'application/json',
+  //     '.map': 'application/json',
+  //     '.png': 'image/png',
+  //     '.jpg': 'image/jpeg',
+  //     '.gif': 'image/gif',
+  //     '.svg': 'image/svg+xml'
+  //   };
+  //
+  //
+  //   // If it's not the GET method, let the default process handle it
+  //   if (req.method !== 'GET') return false;
+  //
+  //   let fileUrl = req.url;
+  //   if (fileUrl === '/') fileUrl = '/index.html';
+  //   const filePath = path.resolve(__dirname, "../webclient/" + fileUrl);
+  //   const fileExt = path.extname(filePath);
+  //   const mimeType = mimeTypes[fileExt];
+  //
+  //   console.log(fileUrl, filePath);
+  //
+  //   // If it's not the right file extension, let the default process handle it
+  //   // if (!Server.ALLOWED_STATIC_FILE_EXTENSIONS.includes(fileExt)) return false;
+  //   if (!mimeType) return false;
+  //
+  //
+  //   return new Promise(resolve => {
+  //
+  //     fs.access(filePath, fs.constants.F_OK, err => {
+  //
+  //       // If it's not an existing file, let the default process handle it
+  //       if (err) {
+  //         resolve(false);
+  //       } else {
+  //         res.statusCode = 200;
+  //         res.setHeader('Content-Type', mimeType);
+  //         fs.createReadStream(filePath).pipe(res);
+  //         resolve(true);
+  //       }
+  //
+  //     });
+  //
+  //   })
+  //
+  // }
 
 
 }
