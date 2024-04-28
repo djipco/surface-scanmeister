7ce686c451de8bcf9c789d8394699fc5f289f49f
Jean-Philippe Côté <jp@djip.co>
2024-04-28 11:18:03 -0400

    Add support for resolution param

--- src/Server.js
+++ src/Server.js
@@ -2,6 +2,7 @@
 import {Configuration as config} from "../config/Configuration.js";
 import http from 'node:http';
 import express from 'express';
+import qs from 'qs';
 
 // Application imports
 import {logError, logInfo, logWarn} from "./Logger.js";
@@ -12,6 +13,7 @@ export class Server extends EventEmitter {
 
   // Valid API commands (first part of the URL)
   static COMMANDS = ["scan"];
+  static RESOLUTIONS = [75, 100, 150, 300, 600, 1200, 2400, 4800];
 
   // Acceptable static files to be served
   static ALLOWED_STATIC_FILE_EXTENSIONS = [".html", ".css", ".js", ".png", ".jpg"]
@@ -28,6 +30,36 @@ export class Server extends EventEmitter {
     super();
   }
 
+  #parseHttpRequest(request) {
+
+    // Result object
+    const parsed = {};
+
+    // Construct a URL object for easier parsing of the command, channel and other parameters.
+    const url = new URL(request.url, `http://${request.headers.host}`);
+    const segments = url.pathname.split('/').slice(1);
+
+    // Check validity of request (expecting at least one valid command)
+    if (segments.length < 1 || ! Server.COMMANDS.includes(segments[0]) ) {
+      throw new Error(`Invalid command received: ${url.href}`);
+    }
+
+    // If we make it here, command is valid. Channel defaults to 1 if not specified.
+    parsed.command = segments[0];
+    parsed.channel = parseInt(segments[1]) || 1;
+
+    // Parse query string for parameters
+    const queryString = qs.parse(url.search);
+
+    // Valdiate resolution
+    if (Server.RESOLUTIONS.includes(parseInt(queryString.resolution))) {
+      parsed.resolution = parseInt(queryString.resolution);
+    }
+
+    return parsed;
+
+  }
+
   async #onHttpRequest(request, response)  {
 
     // Set headers for CORS
@@ -36,22 +68,16 @@ export class Server extends EventEmitter {
     response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
     response.setHeader('Access-Control-Allow-Headers', '*');
 
-    // Parse the path of the URL and split it into segments
-    const url = new URL(request.url, `http://${request.headers.host}`);
-    const segments = url.pathname.split('/').slice(1);
-
-    const command = segments[0];
-    const channel = parseInt(segments[1]) || 1;
-
-    // Check validity of request (expecting /scan or /scan/x where x is the channel number expressed
-    // as an int). When the channel is not specified, channel 0 is used.
-    if (
-      segments.length < 1 ||
-      ! Server.COMMANDS.includes(command)
-    ) {
+    // Try to parse the request
+    let parsed;
+    try {
+      parsed = this.#parseHttpRequest(request);
+      console.log(parsed);
+    } catch (err) {
       logInfo(
-        `Invalid request from ${request.socket.remoteAddress}:${request.socket.remotePort}. ` +
-        `Closing connection.`
+        `Invalid request from ${request.socket.remoteAddress}:${request.socket.remotePort}: ` +
+        err +
+        ` Closing connection.`
       );
       response.writeHead(400, {'Content-Type': 'text/plain'});
       response.end('Invalid request');
@@ -59,11 +85,11 @@ export class Server extends EventEmitter {
     }
 
     // Retrieve scanner matching channel and check if it'is already in use.
-    const scanner = this.getScannerByChannel(channel);
+    const scanner = this.getScannerByChannel(parsed.channel);
     if (!scanner) {
       logWarn(
         `The scanning request from ${request.socket.remoteAddress}:${request.socket.remotePort} `+
-        `was canceled because channel ${channel} is out of bounds.`
+        `was canceled because channel ${parsed.channel} is out of bounds.`
       );
       response.writeHead(400, {'Content-Type': 'text/plain'});
       response.end('Channel out of bounds.');
@@ -71,7 +97,7 @@ export class Server extends EventEmitter {
     } else if (scanner.scanning) {
       logWarn(
         `The scanning request from ${request.socket.remoteAddress}:${request.socket.remotePort} `+
-        `was canceled because channel ${channel} is already in use.`
+        `was canceled because channel ${parsed.channel} is already in use.`
       );
       response.writeHead(400, {'Content-Type': 'text/plain'});
       response.end('Channel already in use.');
@@ -83,7 +109,7 @@ export class Server extends EventEmitter {
     logInfo(
       `Valid request received from ${request.socket.remoteAddress}:${request.socket.remotePort}.`
     );
-    const client = new Client(request.socket, {channel});
+    const client = new Client(request.socket, {channel: parsed.channel});
     this.#clients[client.id] = client;
 
     // Quickly send answer in the form of a proper HTTP header (there's no official MIME type for
@@ -92,7 +118,6 @@ export class Server extends EventEmitter {
     response.writeHead(200, {'Content-Type': 'application/octet-stream'});
 
     // Retrieve scanner and set up callbacks
-
     scanner.addOneTimeListener("scancompleted", () => {
       response.end();
       request.removeAllListeners();
@@ -110,7 +135,7 @@ export class Server extends EventEmitter {
     });
 
     // Scan!
-    scanner.scan({pipe: response});
+    scanner.scan({pipe: response, resolution: parsed.resolution});
 
     // Watch if the client unexpectedly closes the request, in which case we must clean up.
     request.once('close', () => {
